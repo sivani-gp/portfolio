658f3ee8b4fdd5ed1ff337252b43865c594bacc1
sivani-gp <sivanigandupilli@gmail.com>
2025-10-31 19:37:34 +0530

    skills_update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nithish | Portfolio Website';
+export const SITE_TITLE = 'Sivani Gandupilli | Portfolio';
 export const SITE_DESCRIPTION = '';
 export const SITE_IMAGE = '/portfolio/webpage.png';
 
